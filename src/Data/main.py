14f6184d8687e326edd4e31308e2d22ae00185c4
qiantian11 <113962881+qiantian11@users.noreply.github.com>
2022-11-17 23:03:30 +0800

    Add files via upload

--- src/Data/main.py
+++ src/Data/main.py
@@ -1,3 +1,19 @@
-# author 54sher-coder
-def main():
-    pass
+from fastapi import FastAPI
+from pc import Danmu
+app = FastAPI()
+
+#获得爬虫数据
+danmu=Danmu(26397089)
+
+@app.get("/pc")
+async def root(): 
+        load=danmu.get_danmu()  
+        return load
+@app.post("/stop")
+async def root():
+        danmu.ans['A']=0
+        danmu.ans['B']=0
+        danmu.ans['C']=0
+        danmu.ans['D']=0
+        danmu.ans['E']=0
+        danmu.Hashmap={}
